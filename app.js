f16b336dad9ec706f058667905c386dd4a96de7b
phnyo <sabanium@gmail.com>
2019-11-12 15:27:55 +0900

    written the code

--- app.js
+++ app.js
@@ -7,7 +7,8 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    if(n == 1) return result;
+    else result = n*factorial(n-1);
     return result;
 }
 const assert = require('assert');
