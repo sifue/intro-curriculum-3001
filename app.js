f21eb40b14161d7e7e7a628d9c9483cace385e1b
yakata66 <ponyo4869yakata398@gmail.com>
2020-05-23 18:53:48 +0900

    implement factorial function

--- app.js
+++ app.js
@@ -6,9 +6,14 @@
 * @returns {Number}
 */
 function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
-    return result;
+    if (n===1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n-1);
+    }
 }
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
