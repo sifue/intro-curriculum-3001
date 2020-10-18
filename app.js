78629162a69073569f58d92a78985f5ee3b2f8aa
Norihisa Kuwajima <58217615+KJiman@users.noreply.github.com>
2020-10-18 16:25:49 +0900

    階乗の実装

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict';
 /**
-* 与えられた自然数の階乗を返す
-* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-* @param {Number} n
-* @returns {Number}
-*/
+ * 与えられた自然数の階乗を返す
+ * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+ * @param {Number} n
+ * @returns {Number}
+ */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    // TODO
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
     return result;
 }
 const assert = require('assert');
